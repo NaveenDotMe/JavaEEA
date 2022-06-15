52c96c9cf20ab72787869780dbb9304d89f4321d
Brock-99 <elmatadordelmar99@gmail.com>
2022-06-15 08:37:27 +0530

    1st Request Called

--- 01_HelloServlet/src/HelloServlet.java
+++ 01_HelloServlet/src/HelloServlet.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,10 @@ import java.io.IOException;
  * Time : 8:23 AM
  * Project Name : JavaEEA
  **/
+@WebServlet(urlPatterns = "/Hello")
 public class HelloServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request Received");
+    }
 }
