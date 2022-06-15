a9cda53a697c3abe111071b6f0279c51c96f592c
Brock-99 <elmatadordelmar99@gmail.com>
2022-06-15 08:40:18 +0530

    Genarated Little Response

--- 01_HelloServlet/src/HelloServlet.java
+++ 01_HelloServlet/src/HelloServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by : Naveen Pabhath
@@ -16,5 +17,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Request Received");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Received");
     }
 }
